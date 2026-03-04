33c72995704aa8fa3305c60852d8270c2d2d91c5
Sergei Terentev <sergei.terentev@aspose.com>
2026-03-04 11:13:03 +0300

    Add example for ParseApi.AIParse (Extracts data from a document using AI based on a simple JSON template.)

---
+++ Examples/GroupDocs.Parser.Cloud.Examples.CSharp/ParseOperations/AIParseDocument.cs
@@ -0,0 +1,38 @@
+using System;
+using GroupDocs.Parser.Cloud.Sdk.Api;
+using GroupDocs.Parser.Cloud.Sdk.Client;
+using GroupDocs.Parser.Cloud.Sdk.Model;
+using GroupDocs.Parser.Cloud.Sdk.Model.Requests;
+
+namespace GroupDocs.Parser.Cloud.Examples.CSharp
+{
+    public class AIParseDocument
+    {
+        public static void Run()
+        {
+            var config = new Configuration(Common.MyAppSid, Common.MyAppKey);
+            var api = new ParseApi(config);
+
+            var options = new AIParseOptions
+            {
+                FileInfo = new FileInfo
+                {
+                    FilePath = "invoices/invoice.pdf",
+                    StorageName = Common.MyStorage
+                },
+                Template = new
+                {
+                    InvoiceNumber = "",
+                    InvoiceDate = "",
+                    TotalAmount = ""
+                }
+            };
+
+            var request = new AIParseRequest(options);
+            var result = api.AIParse(request);
+
+            Console.WriteLine("AI Parse result:");
+            Console.WriteLine(result);
+        }
+    }
+}
